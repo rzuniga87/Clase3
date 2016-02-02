58c4379528e7929bc1e1a92e844cde5813e34e72
rzuniga87 <rzuniga87@gmail.com>
2016-02-01 19:00:25 -0600

    Clase 3

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3
+{
+    class Program
+    {
+        public class Persona
+        {
+            public string name;
+            public Persona()
+            {
+                name = "Unknown";
+            }
+            public Persona(string nm)
+            {
+                name = nm;
+            }
+            public void SetName(string NewName)
+            {
+                name = NewName;
+            }
+        }
+
+        static void Main()
+        {
+            Persona persona1 = new Persona();
+            Console.WriteLine(persona1.name);
+
+            persona1.SetName("Rigoberto Zuniga");
+            Console.WriteLine(persona1.name);
+
+            Persona persona2 = new Persona("Juan Zuniga");
+            Console.WriteLine(persona2.name);
+
+            Console.WriteLine("Prescione cualquier tecla para continuar.");
+            Console.ReadKey();
+        }
+    }
+}
